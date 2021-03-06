21c272a0cd82292def9de5b4758dddd149561e60
Kristian Sandström <kristian.sandstrom@qbank.se>
2021-03-06 12:03:14 +0100

    card model & django admin api

--- demo_env/backend/magicapi/admin.py
+++ demo_env/backend/magicapi/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Card
+
+class CardAdmin(admin.ModelAdmin):
+	list_display = (
+		'name',
+        'text',
+        'manaCost',
+        'convertedManaCost',
+        'colors',
+        'colorIdentity',
+        'type',
+        'superTypes',
+        'types',
+        'subTypes',
+        'rarity',
+        'power',
+        'toughness',
+        'imageUrl',
+        'rulings',
+	)
 
 # Register your models here.
+
+admin.site.register(Card, CardAdmin)
--- demo_env/backend/magicapi/models.py
+++ demo_env/backend/magicapi/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Card(models.Model):
+	name = models.CharField(max_length=120)
+	text = models.TextField()
+	manaCost = models.CharField(max_length=10)
+	convertedManaCost = models.IntegerField()
+	colors = models.JSONField()
+	colorIdentity = models.JSONField()
+	type = models.CharField(max_length=120)
+	superTypes = models.JSONField()
+	types = models.JSONField()
+	subTypes = models.JSONField()
+	rarity = models.CharField(max_length=120)
+	power = models.IntegerField()
+	toughness = models.IntegerField()
+	imageUrl = models.CharField(max_length=250)
+	rulings = models.JSONField()
+
+	def _str_(self):
+		return self.name
