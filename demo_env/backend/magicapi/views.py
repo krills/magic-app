b0876654a5b9858e1c4a73440db1a7745abed364
Kristian Sandström <kristian.sandstrom@qbank.se>
2021-03-06 12:58:26 +0100

    passing remote data through /list endpoint

--- demo_env/backend/magicapi/views.py
+++ demo_env/backend/magicapi/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from .serializers import CardSerializer
 from .models import Card
 import json
+import requests
+import pprint
 
 # Create your views here.
 
 class CardView(viewsets.ModelViewSet):
-    serializer_class = CardSerializer
-    queryset = Card.objects.all()
+	serializer_class = CardSerializer
+	queryset = Card.objects.all()
 
-    def list(self, response):
-    	return Response(json.dumps([]))
+	magicApiUrl = 'http://api.magicthegathering.io/v1/'
+
+	def list(self, response, *args, **kwargs):
+		cards = requests.get(
+			self.magicApiUrl + 'cards',
+			params = {
+				'page': self.request.GET.get('page', default=1)
+			}
+		)
+		return Response(cards.json())
+
+	def retrieve(self, response, *args, **kwargs):
+		return Response('hello get')
+
+	@action(detail=False)
+	def custom(self, response):
+		return Response('hello custom')
